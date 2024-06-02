4c48a8683c0a9810616bf99d5c5b01a63802bc2e
Vũ Huỳnh Minh <36500047+vhm205@users.noreply.github.com>
2024-06-02 12:47:39 +0700

    fix: variable unused

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,10 +2,9 @@ import { AuthContext, AuthContextProps } from "@/contexts";
 import { useContext } from "react";
 
 export const useAuth = (): AuthContextProps => {
-	const context = useContext(AuthContext);
-  const uid = localStorage.getItem('uid');
-	if (!context) {
-		throw new Error("useAuth must be used within an AuthProvider");
-	}
-	return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
